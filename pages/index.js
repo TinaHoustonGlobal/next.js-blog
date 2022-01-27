9ed62aac0c0449974ed488b2769a482309d57ece
TinaHoustonGlobal <christina.houston@global.com>
2022-01-27 17:40:31 +0000

    making a change to the homepage

--- pages/index.js
+++ pages/index.js
@@ -33,7 +33,7 @@ export default function Home({ allPostsData }) {
           .
         </p>
         <p>
-          (This is a sample website - you’ll be building a site like this on{" "}
+          (This is a sample website - built by following {" "}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
         </p>
       </section>
